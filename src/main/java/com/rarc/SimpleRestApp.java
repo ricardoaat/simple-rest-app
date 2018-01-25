8b8fc4fc9e32a380c42f58d58fb6a05f30f03efb
Ric Arcila <ricardo.arcila@globant.com>
2018-01-24 22:12:26 -0500

    db changes

--- src/main/java/com/rarc/SimpleRestApp.java
+++ src/main/java/com/rarc/SimpleRestApp.java
@@ -1,11 +1,12 @@
 package com.rarc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimpleRestApp extends SpringBootServletInitializer{
+public class SimpleRestApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleRestApp.class, args);
